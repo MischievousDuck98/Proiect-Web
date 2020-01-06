580ed04d400033534e9ae59fcb8e96e2198d3482
MischievousDuck98 <48488339+MischievousDuck98@users.noreply.github.com>
2020-01-06 21:10:46 +0200

    Add files via upload

---
+++ resources/misc_scripts/input_coloring.js
@@ -0,0 +1,7 @@
+function keydown() {
+    document.getElementById("colored").style.backgroundColor = "lime";
+  }
+  
+function keyup() {
+    document.getElementById("colored").style.backgroundColor = "white";
+}
